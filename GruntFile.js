49cf7b2b81a2785814f502e5ba45ae1492c605d8
AbhishekGupta0402 <abhishekedu4979@gmail.com>
2020-04-26 15:46:28 +0530

    css and js minify and uglify with grunt

--- GruntFile.js
+++ GruntFile.js
@@ -5,7 +5,10 @@ module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
     // Automatically load required Grunt tasks
-    require('jit-grunt')(grunt);
+    //    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
     // Define the configuration for all the tasks
     grunt.initConfig({
@@ -36,11 +39,155 @@ module.exports = function (grunt) {
                     }
                 }
             }
+        },
+
+        copy: {
+            html: { //copy html files to dist folder
+                files: [
+                    {
+                        //for html
+                        expand: true,
+                        dot: true,
+                        cwd: './', //current working directory
+                        src: ['*.html'],
+                        dest: 'dist'
+                }]
+            },
+            fonts: { //copy fonts files to dist folder
+                files: [
+                    {
+                        //for font-awesome
+                        expand: true,
+                        dot: true,
+                        cwd: 'node_modules/font-awesome',
+                        src: ['fonts/*.*'],
+                        dest: 'dist'
+                }]
+            }
+        },
+
+        clean: { //clean the dist folder
+            build: {
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true, // Enable dynamic expansion
+                    cwd: './', // Src matches are relative to this path
+                    src: ['img/*.{png,jpg,gif}'], // Actual patterns to match
+                    dest: 'dist/' // Destination path prefix
+                }]
+            }
+        },
+
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html', 'aboutus.html', 'index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0,
+                                    rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+
+        // Concat
+        concat: {
+            options: {
+                separator: ';'
+            },
+
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        // Uglify
+        uglify: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        cssmin: {
+            dist: {}
+        },
+
+        // Filerev
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+
+            release: {
+                // filerev:release hashes(md5) all assets (images, js and css )
+                // in dist directory
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css',
+                    ]
+                }]
+            }
+        },
+
+        // Usemin
+        // Replaces all assets with their revved version in html and css files.
+        // options.assetDirs contains the directories for finding the assets
+        // according to their relative paths
+        usemin: {
+            html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css', 'dist/js']
+            }
+        },
+
+        htmlmin: { // Task
+            dist: { // Target
+                options: { // Target options
+                    collapseWhitespace: true
+                },
+                files: { // Dictionary of files
+                    'dist/index.html': 'dist/index.html', // 'destination': 'source'
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html': 'dist/aboutus.html',
+                }
+            }
         }
     });
 
     grunt.registerTask('css', ['sass']); //to run individual task
     grunt.registerTask('default', ['browserSync', 'watch']); //always do the watch task at the end bcoz it stops all other tasks after it
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
 
 
 };
